7656d0442335a42a42eb4e67582607a9d14935ab
clever.ch <clever.chuky@gmail.com>
2020-04-11 20:49:01 -0300

    Update Entity.java

--- src/main/java/root/utilities/Entity.java
+++ src/main/java/root/utilities/Entity.java
@@ -1,8 +1,15 @@
 package root.utilities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class Entity {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	public long getId() {
