2c797f086b78d1c797da2270d2a1b95b21d79abb
clever.ch <clever.chuky@gmail.com>
2020-04-11 18:09:10 -0300

    Create Care.java

---
+++ src/main/java/root/model/Care.java
@@ -0,0 +1,16 @@
+package root.model;
+
+public class Care extends Service {
+
+	private int careTime;
+
+	
+	public int getCareTime() {
+		return careTime;
+	}
+
+	public void setCareTime(int careTime) {
+		this.careTime = careTime;
+	}
+
+}
