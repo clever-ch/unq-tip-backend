df2d70c56545b6d93bb5c86c03eb6d876d3dc1c7
nicolaspintos <nico.pintos05@gmail.com>
2020-04-14 21:43:18 -0300

    Test Dog OK

--- src/test/java/model/factories/DogFactory.java
+++ src/test/java/model/factories/DogFactory.java
@@ -16,5 +16,40 @@ public class DogFactory {
 
         return newDog;
     }
+
+    public static Dog createDogWithAnimalType(AnimalType animalType){
+        Dog newDog = new Dog();
+        newDog.setAnimalType(animalType);
+
+        return newDog;
+    }
+
+    public static Dog createDogWithBreed(String breed){
+        Dog newDog = new Dog();
+        newDog.setBreed(breed);
+
+        return newDog;
+    }
+
+    public static Dog createDogWithSize(String size){
+        Dog newDog = new Dog();
+        newDog.setSize(size);
+
+        return newDog;
+    }
+
+    public static Dog createDogWithAge(String age){
+        Dog newDog = new Dog();
+        newDog.setAge(age);
+
+        return newDog;
+    }
+
+    public static Dog createDogWithDescription(String description){
+        Dog newDog = new Dog();
+        newDog.setDescription(description);
+
+        return newDog;
+    }
 }
 
