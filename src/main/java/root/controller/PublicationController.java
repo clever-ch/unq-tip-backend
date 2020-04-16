0c2fed7271fedfba5c4e40c8a00aa803b798180d
clever.ch <clever.chuky@gmail.com>
2020-04-16 19:54:03 -0300

    Update PublicationController.java

--- src/main/java/root/controller/PublicationController.java
+++ src/main/java/root/controller/PublicationController.java
@@ -1,11 +1,13 @@
 package root.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import root.DTO.PublicationDTO;
 import root.model.Publication;
 import root.repository.PublicationRepository;
 
@@ -19,7 +21,37 @@ public class PublicationController {
 	private PublicationRepository publicationRepository;
 	
 	@GetMapping("/publications")
-	public List<Publication> getAllPublications() {
-		return publicationRepository.findAll();
+	public List<PublicationDTO> getAllPublications() {
+		
+		List<PublicationDTO> publicationsDTO = new ArrayList<PublicationDTO>();
+		List<Publication> publications = publicationRepository.findAll();
+		
+		for (Publication publication : publications) {
+			publicationsDTO.add(ConvertPublicationToPublicationDTO(publication));
+		}
+		
+		return publicationsDTO;
+	}
+	
+	private PublicationDTO ConvertPublicationToPublicationDTO(Publication publication)
+	{
+		PublicationDTO publicationDTO = new PublicationDTO();
+		publicationDTO.Id = publication.getId();
+		publicationDTO.Breed = publication.getAnimal().getBreed();
+		publicationDTO.Size = publication.getAnimal().getSize();
+		publicationDTO.Age = publication.getAnimal().getAge();
+		publicationDTO.Description = publication.getAnimal().getDescription();
+		
+		publicationDTO.UserName = publication.getUser().getUserName();
+		publicationDTO.Email = publication.getUser().getEmail();
+		
+		publicationDTO.Location = publication.getLocation();
+		publicationDTO.PublicationType = publication.getType().ordinal();
+		publicationDTO.PublicationStatus = publication.getStatus().ordinal();
+		publicationDTO.Adrees = publication.getAddress();
+		publicationDTO.Specification = publication.getSpecification();
+		publicationDTO.Photos = publication.getPhotos();
+		
+		return publicationDTO;
 	}
 }
