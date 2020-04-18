968055592c6d219bae7b3cd95e2807f498cbace9
clever.ch <clever.chuky@gmail.com>
2020-04-18 17:40:13 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,19 @@
---Sección Animales
-insert into Cat	values (1,'edadGato', 3, 'razaGato', 'descriptionGato', 'tamañoGato');
+--Sección Accounts
+insert into Account values (1, 'address1', 'location1', 'name1', 'surname1', 'telephone1');
+insert into Account values (2, 'address2', 'location2', 'name2', 'surname2', 'telephone2');
+
+
+--Sección Users
+insert into User values (1, 'emailUser1', 'UserName1', 1);
+insert into User values (2, 'emailUser2', 'UserName2', 2);
+
+
+--Sección Animals
+insert into Animal values (1,'edadGato1', 1, 'razaGato1', 'descriptionGato1', 'sizeGato1');
+insert into Animal values (2,'edadPerro1', 2, 'razaPerro1', 'descriptionPerro1', 'sizePerro1');
+insert into Animal values (3,'edadPajaro1', 0, 'razaPajaro1', 'descriptionPajaro1', 'sizePajaro1');
+
+
+--Sección Publications
+insert into Publication values (1, 'address1', 'location1', 0, 0, 'specification1', 1, 1);
+insert into Publication values (2, 'address2', 'location2', 0, 1, 'specification2', 2, 2);
