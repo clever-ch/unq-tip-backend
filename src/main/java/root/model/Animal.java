cd7a08ccbb28c4ee49efbdf890307f63ea895869
clever.ch <clever.chuky@gmail.com>
2020-04-18 17:38:37 -0300

    Update Animal.java

--- src/main/java/root/model/Animal.java
+++ src/main/java/root/model/Animal.java
@@ -2,22 +2,18 @@ package root.model;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import root.constants.AnimalType;
 import root.utilities.Entity;
 
 @javax.persistence.Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Animal extends Entity {
 	
 	@Enumerated(EnumType.STRING)
 	private AnimalType animalType;
-	private String breed;
-	private String size;
 	private String age;
+	private String breed;
 	private String description;
-	
+	private String size;	
 	
 	public String getBreed() {
 		return breed;
