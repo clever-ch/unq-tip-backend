62c0e9ff9a9577169a8d5fe19e3029290e437e62
clever.ch <clever.chuky@gmail.com>
2020-04-23 19:01:34 -0300

    Update TestPublication.java

--- src/test/java/model/TestPublication.java
+++ src/test/java/model/TestPublication.java
@@ -1,49 +1,25 @@
 package model;
 
-import model.factories.CatFactory;
-import model.factories.DogFactory;
 import model.factories.PublicationFactory;
 import model.factories.UserFactory;
 import org.junit.Test;
 import root.constants.PublicationStatus;
 import root.constants.PublicationType;
-import root.model.Cat;
-import root.model.Dog;
 import root.model.Publication;
 import root.model.User;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
 public class TestPublication {
 
-    @Test
-    public void testIsValidPublication(){
-
-        Cat aCat = CatFactory.anyCat();
-        User aUser = UserFactory.anyUser();
-
-        Publication aPublication = PublicationFactory.createCompletePublication(aCat, aUser, "Quilmes", PublicationType.LOST,
-                PublicationStatus.ACTIVE, "Mitre", "Esto es un especificacion");
-
-        assertTrue(aPublication.isValidPublication());
-    }
-
-    @Test
-    public void testNotIsEmptyAnimal() {
-        Dog aDog = DogFactory.anyDog();
-
-        Publication aPublication = PublicationFactory.createPublicationWithAnimal(aDog);
-        assertFalse(aPublication.isEmptyAnimal());
-    }
 
     @Test
     public void testNotIsEmptyUser() {
         User aUser = UserFactory.anyUser();
 
         Publication aPublication = PublicationFactory.createPublicationWithUser(aUser);
-        assertTrue(aPublication.hasUser());
+        assertTrue(aPublication.isEmptyUser());
     }
 
     @Test
@@ -57,21 +33,21 @@ public class TestPublication {
     public void testHasAType(){
         Publication aPublication = PublicationFactory.createPublicationWithType(PublicationType.LOST);
 
-        assertTrue((aPublication.hasAType()));
+        assertTrue((aPublication.hasAPublicationType()));
     }
 
     @Test
     public void testHasAStatusActive(){
         Publication aPublication = PublicationFactory.createPublicationWithStatus(PublicationStatus.ACTIVE);
 
-        assertTrue(aPublication.hasAStatus() && aPublication.getStatus() == PublicationStatus.ACTIVE);
+        assertTrue(aPublication.hasAPublicationStatus() && aPublication.getPublicationStatus() == PublicationStatus.ACTIVE);
     }
 
     @Test
     public void testHasAStatusInactive(){
         Publication aPublication = PublicationFactory.createPublicationWithStatus(PublicationStatus.INACTIVE);
 
-        assertTrue(aPublication.hasAStatus() && aPublication.getStatus() == PublicationStatus.INACTIVE);
+        assertTrue(aPublication.hasAPublicationStatus() && aPublication.getPublicationStatus() == PublicationStatus.INACTIVE);
     }
 
     @Test
