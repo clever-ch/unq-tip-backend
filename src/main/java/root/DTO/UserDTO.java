adf2c2f27aa2b954f3552abab3b2ca9f43efa04f
clever.ch <clever.chuky@gmail.com>
2020-04-26 18:56:28 -0300

    Create UserDTO.java

---
+++ src/main/java/root/DTO/UserDTO.java
@@ -0,0 +1,12 @@
+package root.DTO;
+
+public class UserDTO {
+	
+	public long Id;
+	public String UserName;
+	public String Email;	
+	public String AccountName;
+	public String AccountSurName;
+	public String AccountTelephone;
+	public String AccountAddress;
+}
