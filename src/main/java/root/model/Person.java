b01c7de54d934de33b267cd2efacb818f6a2adc6
clever.ch <clever.chuky@gmail.com>
2020-05-06 17:22:45 -0300

    Create Person.java

---
+++ src/main/java/root/model/Person.java
@@ -0,0 +1,45 @@
+package root.model;
+
+import root.utilities.Entity;
+
+@javax.persistence.Entity
+public class Person extends Entity {
+	
+	private String address;
+	private String location;
+	private String name;
+	private String surName;
+	private String telephone;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
