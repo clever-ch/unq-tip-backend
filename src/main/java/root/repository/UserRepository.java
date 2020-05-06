b30e6a79a92651769fdb69828dc58f3de9dec7ea
clever.ch <clever.chuky@gmail.com>
2020-05-06 17:24:18 -0300

    Create UserRepository.java

---
+++ src/main/java/root/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package root.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import root.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
