6e116ab8d93428cf8baf015c720e8ba578d72b66
clever.ch <clever.chuky@gmail.com>
2020-05-10 17:44:36 -0300

    Update UserRepository.java

--- src/main/java/root/repository/UserRepository.java
+++ src/main/java/root/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package root.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import root.model.User;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	
+	@Query("select u from User u where u.userName = ?1 and u.password = ?2")
+	  User findByUserNameAndPassword(String userName, String password);
 }
