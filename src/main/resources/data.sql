cc320e7357930f388da1a9f0a9fc3eac8ebebefd
clever.ch <clever.chuky@gmail.com>
2020-05-25 13:30:07 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,35 +24,45 @@ insert into Animal values (6,'5 años', 0, 'Siames', 'Hembra, ojos celestes clar
 insert into Animal values (7,'3 años', 0, 'Guacamayo', 'Color amarillo y celeste, le falta una pluma abajo del ala derecha', 'aproximadamente Alto 40cm');
 
 
---Sección Publications
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+--Sección Publication Found
 insert into Publication values (1, 'location1', 'Bernal - Maipú 327', 'Fue encontrado por esos alrededores', 0, 0, 1, 1);
-insert into Publication values (2, 'location2', 'Berazategui - Av. Mitre', 'Se la vió por última vez cerca de la UAI', 0, 1, 2, 2);
 insert into Publication values (3, 'location3', 'Quilmes - Av. Mitre', 'Se lo encontró en la estacion de servicio de calle Bransen', 0, 0, 3, 3);
-insert into Publication values (4, 'location4', 'Reconquista - CABA', 'Se la vió por última vez cerca de Avenida Cordoba', 0, 1, 4, 4);
 insert into Publication values (5, 'location5', 'Avellaneda - Ramon Franco', 'Se lo rescató en estacion de tren Wilde', 0, 0, 5, 5);
-insert into Publication values (6, 'location6', 'Palermo - Arenales', 'Se la vió por última vez por el barrio', 0, 1, 6, 6);
 insert into Publication values (7, 'location7', 'Florencio Varela - Avenida del Trabajo', 'Fue rescatado cerca de la Universidad Jauretche', 0, 0, 7, 2);
 
 
+--Sección Publication Lost
+insert into Publication values (2, 'location2', 'Berazategui - Av. Mitre', 'Se la vió por última vez cerca de la UAI', 0, 1, 2, 2);
+insert into Publication values (4, 'location4', 'Reconquista - CABA', 'Se la vió por última vez cerca de Avenida Cordoba', 0, 1, 4, 4);
+insert into Publication values (6, 'location6', 'Palermo - Arenales', 'Se la vió por última vez por el barrio', 0, 1, 6, 6);
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 --Sección tabla intermedia PUBLICATION_PHOTOS
-insert into PUBLICATION_PHOTOS values (1, 'https://www.elmundodelgato.com/fotos/78/1219_P1211772.JPG');
-insert into PUBLICATION_PHOTOS values (1, 'https://s3-eu-west-1.amazonaws.com/barkibu-blog/blog+images/como-saber-que-raza-es-mi-gato/gato-tuxis2.png');
-insert into PUBLICATION_PHOTOS values (1, 'https://i.pinimg.com/originals/2e/95/13/2e9513de4ee17692a1f78985b139d1f4.jpg');
-insert into PUBLICATION_PHOTOS values (1, 'https://cdn.pixabay.com/photo/2018/11/27/21/21/cat-3842408_960_720.jpg');
+insert into PUBLICATION_PHOTOS values (1, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2F1219_P1211772.JPG?alt=media&token=eab029d1-4bd5-4554-9f54-1d01a47723df');
+insert into PUBLICATION_PHOTOS values (1, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2F2e9513de4ee17692a1f78985b139d1f4.jpg?alt=media&token=51f3e7aa-ca90-412b-b957-78bc6f11f0cc');
+insert into PUBLICATION_PHOTOS values (1, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Fcat-3842408_960_720.jpg?alt=media&token=8649668b-2e95-46f0-8745-37ec5cb1b5ea');
+insert into PUBLICATION_PHOTOS values (1, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Fgato-tuxis2.png?alt=media&token=05375998-a274-4ce2-bf97-20780afd5b57');
 
-insert into PUBLICATION_PHOTOS values (2, 'https://misanimales.com/wp-content/uploads/2015/12/cocker-spaniel-americano.jpg');
-insert into PUBLICATION_PHOTOS values (2, 'https://3.bp.blogspot.com/-yb6LhqETeMw/UUUSiFQjZMI/AAAAAAAAAB4/rwxx--e21bo/s1600/cachorra+cocker.jpg');
 
-insert into PUBLICATION_PHOTOS values (3, 'https://cdn.pixabay.com/photo/2018/08/15/21/20/mallard-3609130_960_720.jpg');
+insert into PUBLICATION_PHOTOS values (2, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fcocker-spaniel-americano.jpg?alt=media&token=8027bfb6-4b2b-4bf7-aa5b-bdcbd1e20910');
+insert into PUBLICATION_PHOTOS values (2, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fcachorra%20cocker.jpg?alt=media&token=34f32f09-e380-4509-9ea4-ab952367eb03');
 
-insert into PUBLICATION_PHOTOS values (4, 'https://cdn.pixabay.com/photo/2016/03/20/15/37/dog-1268813_960_720.jpg');
-insert into PUBLICATION_PHOTOS values (4, 'https://cdn.pixabay.com/photo/2016/03/20/15/33/dog-1268794_960_720.jpg');
 
-insert into PUBLICATION_PHOTOS values (5, 'https://cdn.pixabay.com/photo/2014/06/08/21/59/american-staffordshire-terrier-365109_960_720.jpg');
-insert into PUBLICATION_PHOTOS values (5, 'https://cdn.pixabay.com/photo/2018/08/19/10/57/american-staffordshire-terrier-3616318_960_720.jpg');
+insert into PUBLICATION_PHOTOS values (3, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Fmallard-3609130_960_720.jpg?alt=media&token=d17247d8-a35d-4660-986a-83ae0bf1d1b6');
 
-insert into PUBLICATION_PHOTOS values (6, 'https://cdn.pixabay.com/photo/2017/07/22/18/25/cat-2529563_960_720.jpg');
-insert into PUBLICATION_PHOTOS values (6, 'https://cdn.pixabay.com/photo/2017/02/15/12/12/cat-2068462_960_720.jpg');
 
-insert into PUBLICATION_PHOTOS values (7, 'https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_960_720.jpg');
-insert into PUBLICATION_PHOTOS values (7, 'https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_960_720.jpg');
+insert into PUBLICATION_PHOTOS values (4, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fdog-1268813_960_720.jpg?alt=media&token=28d6211b-5d78-4afe-931d-9174d7b88079');
+insert into PUBLICATION_PHOTOS values (4, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fdog-1268794_960_720.webp.jpg?alt=media&token=e85175ce-55e9-45f6-b67d-055684fa0a72');
+
+
+insert into PUBLICATION_PHOTOS values (5, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Famerican-staffordshire-terrier-365109_960_720.jpg?alt=media&token=c9572082-1b21-48a9-a11f-f8648e3ca90a');
+insert into PUBLICATION_PHOTOS values (5, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Famerican-staffordshire-terrier-3616318_960_720.jpg?alt=media&token=024d0265-2df7-425d-96e5-b394e94f12e6');
+
+
+insert into PUBLICATION_PHOTOS values (6, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fcat-2529563_960_720.jpg?alt=media&token=67f8fecd-9407-48a8-84e2-f26eb032ff11');
+insert into PUBLICATION_PHOTOS values (6, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-lost%2Fcat-2068462_960_720.jpg?alt=media&token=ce0b16d9-1c64-44c9-987d-8cada6e7b0b8');
+
+
+insert into PUBLICATION_PHOTOS values (7, 'https://firebasestorage.googleapis.com/v0/b/tip-sospets.appspot.com/o/img-publication-found%2Fara-3601194_960_720.jpg?alt=media&token=6ca8a03e-a8c8-4c5f-b451-fba95a9c0451');
