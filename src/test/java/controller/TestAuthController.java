75849cd5e16b2e6638c01c13b9ecab271a1007b8
clever.ch <clever.chuky@gmail.com>
2020-05-31 17:34:08 -0300

    Update TestAuthController.java

--- src/test/java/controller/TestAuthController.java
+++ src/test/java/controller/TestAuthController.java
@@ -5,11 +5,22 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import factories.PersonFactory;
+import factories.UserFactory;
 import modelDTO.LoginDTOFactory;
+import modelDTO.PersonDTOFactory;
+import modelDTO.UserDTOFactory;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import root.DTO.LoginDTO;
+import root.DTO.PersonDTO;
+import root.DTO.UserDTO;
 import root.controller.AuthController;
+import root.model.Person;
+import root.model.User;
 import root.repository.UserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,6 +39,35 @@ public class TestAuthController {
 		LoginDTO loginDTO = LoginDTOFactory.getLoginDTOWithParameters(USERNAME, PASSWORD, USERGUID);
 		when(userRepository.findUserByUserNameAndUserGuid(USERNAME, USERGUID)).thenReturn(null);
 		
-		assertEquals(false, authController.loggedInUserExists(loginDTO));
+		assertFalse(authController.loggedInUserExists(loginDTO));
+	}
+	
+	@Test
+	public void testUserExistsWithUserNameAndUSerGuid() {
+		final String USERNAME = "aUserName", PASSWORD = "", USERGUID = "aUserGuid";
+		
+		LoginDTO loginDTO = LoginDTOFactory.getLoginDTOWithParameters(USERNAME, PASSWORD, USERGUID);
+		User existingUser = UserFactory.createAUserWithUserNamePasswordAndUserGuid(USERNAME, PASSWORD, USERGUID);
+		
+		when(userRepository.findUserByUserNameAndUserGuid(USERNAME, USERGUID)).thenReturn(existingUser);
+		
+		assertTrue(authController.loggedInUserExists(loginDTO));
+	}
+	
+	@Test
+	public void testUserExistsByGuid() {
+		final String USERNAME = "aUserName", PASSWORD = "", USERGUID = "aUserGuid", EMAIL = "";
+		
+		Person person = PersonFactory.anyPerson();
+		User existingUser = UserFactory.createACompleteUser(USERNAME, EMAIL, person);
+		
+		PersonDTO personDTO = PersonDTOFactory.anyPersonDTO();
+		UserDTO existingUserDTO = UserDTOFactory.createACompleteUser(USERNAME, PASSWORD, personDTO);
+		
+		when(userRepository.findUserByUserGuid(USERGUID)).thenReturn(existingUser);
+		
+		ResponseEntity<UserDTO> result = authController.getUserByGuid(USERGUID);
+		assertEquals(existingUserDTO.UserName, result.getBody().UserName);
+		assertEquals(existingUserDTO.UserGuid, result.getBody().UserGuid);
 	}
 }
