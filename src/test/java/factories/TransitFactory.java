181b2f1dffcab384e2b98a48f6e72807bfb2aded
clever.ch <clever.chuky@gmail.com>
2020-05-31 15:15:30 -0300

    Create TransitFactory.java

---
+++ src/test/java/factories/TransitFactory.java
@@ -0,0 +1,70 @@
+package factories;
+
+import root.constants.ServiceStatus;
+import root.constants.UnidOfTime;
+import root.model.Animal;
+import root.model.Transit;
+import root.model.User;
+
+public class TransitFactory {
+
+    public static Transit anyTransitService(){
+        return new Transit();
+    }
+
+    public static Transit createCompleteTransitService(User aUser, ServiceStatus aServiceStatus, Animal aAnimal, String aDescription,
+                                                UnidOfTime aUnidOfTime, int aTransitTime){
+        Transit aTransitService = new Transit();
+        aTransitService.setUser(aUser);
+        aTransitService.setServiceStatus(aServiceStatus);
+        aTransitService.setAnimal(aAnimal);
+        aTransitService.setDescription(aDescription);
+        aTransitService.setUnidOfTime(aUnidOfTime);
+        aTransitService.setTransitTime(aTransitTime);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithUser(User aUser){
+        Transit aTransitService = new Transit();
+        aTransitService.setUser(aUser);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithServiceStatus(ServiceStatus aServiceStatus){
+        Transit aTransitService = new Transit();
+        aTransitService.setServiceStatus(aServiceStatus);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithAnimal(Animal aAnimal){
+        Transit aTransitService = new Transit();
+        aTransitService.setAnimal(aAnimal);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithDescription(String aDescription){
+        Transit aTransitService = new Transit();
+        aTransitService.setDescription(aDescription);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithUnidOfTime(UnidOfTime aUnidOfTime){
+        Transit aTransitService = new Transit();
+        aTransitService.setUnidOfTime(aUnidOfTime);
+
+        return aTransitService;
+    }
+
+    public static Transit createTransitServiceWithTransitTime(int aTransitTime){
+        Transit aTransitService = new Transit();
+        aTransitService.setTransitTime(aTransitTime);
+
+        return aTransitService;
+    }
+
+}
