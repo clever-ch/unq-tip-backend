7a50d797306839ce4c943bc297a102f843c91a7e
clever.ch <clever.chuky@gmail.com>
2020-06-10 17:17:07 -0300

    Create CareDTO.java

---
+++ src/main/java/root/DTO/CareDTO.java
@@ -0,0 +1,15 @@
+package root.DTO;
+
+import root.constants.ServiceStatus;
+import root.constants.UnidOfTime;
+
+public class CareDTO {
+
+	public long Id;
+	public UserDTO UserDTO;
+	
+	public String ServiceDescription;
+	public ServiceStatus ServiceStatus;
+	public UnidOfTime UnidOfTime;
+	public int CareTime;
+}
