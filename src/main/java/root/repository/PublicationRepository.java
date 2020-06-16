f0be2f391b459ee40288d52e6bb21dcd3a14dd7b
clever.ch <clever.chuky@gmail.com>
2020-06-16 18:08:34 -0300

    Update PublicationRepository.java

--- src/main/java/root/repository/PublicationRepository.java
+++ src/main/java/root/repository/PublicationRepository.java
@@ -2,8 +2,11 @@ package root.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import root.model.Publication;
 
 
@@ -21,4 +24,12 @@ public interface PublicationRepository extends JpaRepository<Publication, Long>{
 	
 	@Query("select p from Publication p where p.publicationType = 0 and p.user.id = ?1")
 	List<Publication> findAllPublicationsFoundByIdUser(Long idUser);
+	
+	@Query("select p from Publication p where p.user.id = null")
+	Publication getLastPublicationCreated();
+	
+	@Transactional
+	@Modifying
+	@Query("update Publication p set p.user.id = ?1 where p.id = ?2")
+	void updateUser(Long idUser, Long idPublicacion);
 }
