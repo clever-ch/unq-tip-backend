dce6fb7f581eb3d5a1b2ec2bc87ff62c11811e9a
clever.ch <clever.chuky@gmail.com>
2020-06-21 18:08:17 -0300

    Update UserController.java

--- src/main/java/root/controller/UserController.java
+++ src/main/java/root/controller/UserController.java
@@ -13,16 +13,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import root.DTO.PersonDTO;
 import root.DTO.UserDTO;
 import root.controller.exceptions.EmailAlreadyCreatedException;
 import root.controller.exceptions.EmailInvalidException;
 import root.controller.exceptions.UserAlreadyCreatedExceptions;
 import root.exceptions.InvalidUserException;
-import root.model.Person;
 import root.model.User;
 import root.repository.PersonRepository;
 import root.repository.UserRepository;
+import root.transformers.UserTransformer;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -39,26 +38,26 @@ public class UserController {
 	@PostMapping("/createUser")
 	public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) throws EmailAlreadyCreatedException, UserAlreadyCreatedExceptions {
 
-		User user = ConvertUserDTOToUser(userDTO);
+		ValidateEmail(userDTO.Email);
 		
 		List<User> users = userRepository.findAll();
 		
 		for (User us : users) {
-			//Controlo que el userName no este creado ya.
-			if(us.getUserName().equals(user.getUserName())) throw new UserAlreadyCreatedExceptions();
-			
-			//Controlo que no haya otro usuario con el mismo mail.
-			if(us.getEmail().equals(user.getEmail())) throw new EmailAlreadyCreatedException();
-		}	
+			if(us.getUserName().equals(userDTO.UserName)) throw new UserAlreadyCreatedExceptions();
+
+			if(us.getEmail().equals(userDTO.Email)) throw new EmailAlreadyCreatedException();
+		}
 		
-		//Controlo que todos los campos  esten completos y no posean null o string vacios.
-		if (user.isValidUser()){ 	
+		if (userDTO.isValidUserDTO()){
+			
+			User user = UserTransformer.ConvertUserDTOToUser(userDTO);
+			user.setUserGuid(UUID.randomUUID().toString());
 			userRepository.save(user);
+			
 			return ResponseEntity.ok(user);
 		} else throw new InvalidUserException("Usuario incompleto");
 	}
 	
-	//Metodo para validar que el mail este bien escrito
 	private boolean isValidEmail(String email) throws EmailInvalidException {
 		// Patrón para validar el email
 				Pattern pattern = Pattern
@@ -68,53 +67,24 @@ public class UserController {
 				return mather.find();
 	}
 	
-	private User ConvertUserDTOToUser(UserDTO userDTO) {
-		User user = new User();
-		
-		user.setUserName(userDTO.UserName);
-		user.setPassword(userDTO.Password);
-		
-		//Controlo que el mail sea valido antes de hacer el save
-		if(isValidEmail(userDTO.Email)) {
-			user.setEmail(userDTO.Email);
-		} else throw new EmailInvalidException();
-		
-		user.setUserGuid(UUID.randomUUID().toString());
-		user.setPerson(ConvertPersonDTOToPerson(userDTO.PersonDTO));
-		
-		return user;
-	}
-
-	private Person ConvertPersonDTOToPerson(PersonDTO personDTO) {
-		Person person = new Person();
-		
-		person.setName(personDTO.Name);
-		person.setSurName(personDTO.SurName);
-		person.setTelephone(personDTO.Telephone);
-		person.setAddress(personDTO.Address);
-		person.setLocation(personDTO.Location);
-		
-		return person;
-	}
-	
 	@PutMapping("/editProfile")
 	public ResponseEntity<User> editProfile(@Valid @RequestBody UserDTO userDTO) {
 		
-		User user = userRepository.findUserByUserGuid(userDTO.UserGuid);
-		
-		//Controlo que el mail sea valido antes de hacer el save
-		if(isValidEmail(userDTO.Email)) {
-					user.setEmail(userDTO.Email);
-				} else throw new EmailInvalidException();
-	
-		user.setUserName(userDTO.UserName);
-		user.setPassword(userDTO.Password);
-		user.setPerson(null);				//Se setea en null para no pisar la persona en el update
-		
-		if (user.isValidUser()){
-			SaveOrUpdateUser(userDTO, user);			
+		ValidateEmail(userDTO.Email);
+
+		if (userDTO.isValidUserDTO()){
+			
+			User user = userRepository.findUserByUserGuid(userDTO.UserGuid);
+			UserTransformer.LoadUserByUserDTO(userDTO, user);			
+			SaveOrUpdateUser(userDTO, user);
+			
 			return ResponseEntity.ok(user);
 		} else throw new InvalidUserException("Usuario incompleto");
+	}
+
+	private void ValidateEmail(String eMail) {
+		if(!isValidEmail(eMail))
+			throw new EmailInvalidException();
 	}
 
 	private void SaveOrUpdateUser(UserDTO userDTO, User user) {
