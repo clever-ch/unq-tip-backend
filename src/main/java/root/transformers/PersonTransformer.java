d36257385f27459462345108f7fe471b7ba0d5c2
clever.ch <clever.chuky@gmail.com>
2020-06-21 18:08:35 -0300

    Create PersonTransformer.java

---
+++ src/main/java/root/transformers/PersonTransformer.java
@@ -0,0 +1,32 @@
+package root.transformers;
+
+import root.DTO.PersonDTO;
+import root.model.Person;
+import root.model.Publication;
+
+public class PersonTransformer {
+
+	public static Person ConvertPersonDTOToPerson(PersonDTO personDTO) {
+		Person person = new Person();
+		person.setAddress(personDTO.Address);
+		person.setLocation(personDTO.Location);
+		person.setName(personDTO.Name);
+		person.setSurName(personDTO.SurName);
+		person.setTelephone(personDTO.Telephone);
+		
+		return person;
+	}
+	
+	public static PersonDTO GetPersonDTOByPublication(Publication publication)
+	{
+		PersonDTO personDTO = new PersonDTO();
+		
+		personDTO.Name = publication.getUser().getPerson().getName();
+		personDTO.SurName = publication.getUser().getPerson().getSurName();
+		personDTO.Telephone = publication.getUser().getPerson().getTelephone();
+		personDTO.Address = publication.getUser().getPerson().getAddress();
+		personDTO.Location = publication.getUser().getPerson().getLocation();
+		
+		return personDTO;
+	}
+}
